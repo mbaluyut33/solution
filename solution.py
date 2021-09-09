27e4eb7c8a373beffb6dab969100e1af223268e7
mbaluyut33 <79108362+mbaluyut33@users.noreply.github.com>
2021-09-09 01:10:19 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,7 @@
 def welcome_assignment_answers(question):
     # The student doesn't have to follow the skeleton for this assignment.
     # Another way to implement it is using "case" statements similar to C.
+    answer = None
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
