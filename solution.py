1a5f1e69951ac5543d8e6406ac007db569c53400
mbaluyut33 <79108362+mbaluyut33@users.noreply.github.com>
2021-09-09 01:15:20 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     elif question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         #you have to hunt for this one in slack* (DO NOT INCLUDE THE * or red text in the question)
         answer = "mTLS"
